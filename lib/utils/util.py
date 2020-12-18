700248a71074ce4d9b8759b98570b6ee2a72e442
lepodl <luckyzlb15@163.com>
2020-12-18 20:51:47 +0800

    增加了一个日志记录，以及使用日志和保存网络模板

---
+++ lib/utils/util.py
@@ -0,0 +1,54 @@
+import glob
+import os
+import logging
+import re
+import datetime
+import torch
+
+
+def get_logger(verbosity=1, name=None):
+    str_date = datetime.datetime.now().strftime('%Y-%m-%d')
+    log_path = os.path.join(os.getcwd(), 'log')
+    log_name = os.path.join(log_path, (str_date+'.log'))
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    if not os.path.exists(log_name):
+        open(log_name, mode='w+')
+
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s][line:%(lineno)d][%(levelname)s] %(message)s"
+    )
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+
+    fh = logging.FileHandler(log_name, "a+")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
+
+
+def findLastCheckpoint(saveDir, model):
+    file_dir = os.path.join(saveDir, model)
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    file_list = glob.glob(os.path.join(file_dir, ('model_*.pth')))
+    if file_list:
+        epochs_exist = []
+        for file_ in file_list:
+            result = re.findall(".*model_(.*).pth.*", file_)
+            epochs_exist.append(int(result[0]))
+        initial_epoch = max(epochs_exist)
+    else:
+        initial_epoch = 0
+    model = None
+    if initial_epoch > 0:
+        model = torch.load(os.path.join(
+            saveDir, model, 'model_%03d.pth' % initial_epoch))
+    return (initial_epoch, model)
+
--- main_train.py
+++ main_train.py
@@ -1,103 +1,219 @@
-from torch import nn
-from torch.utils.data.dataloader import DataLoader
-from lib.utils.dataset import FabricDataset
+import os
+import ssl
+
 import torch
 import torchvision
-from torchvision import transforms
-from torch import cuda
-import ssl
+from torch import cuda, nn, optim
+from torch.utils.data.dataloader import DataLoader
+from lib.utils.util import get_logger
+from lib.utils.data_split import createDIYDataset
+from lib.utils.dataset import FabricDataset
+
 ssl._create_default_https_context = ssl._create_unverified_context
 device = torch.device('cuda:0' if cuda.is_available() else 'cpu')
 
+logger = get_logger()
+
 
 class fabric(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=15):
         super(fabric, self).__init__()
-        self.pretrained = nn.Sequential(
-            torchvision.models.vgg11(pretrained=True).features,
-            nn.AdaptiveAvgPool2d((7, 7)))
-
-        self.fc = nn.Linear(512 * 7 * 7, 4)
+        x = torchvision.models.AlexNet(num_classes)
+        # self.feature = nn.Sequential(
+        #     nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.Conv2d(64, 192, kernel_size=5, padding=2),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.Conv2d(192, 384, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(384, 256, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True)
+        # )
+        self.feature = x.features
+        self.pool = x.avgpool
+        # self.avgpool = nn.Sequential(
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.AdaptiveAvgPool2d((6, 6))
+        # )
+        self.classifier = x.classifier
 
     def forward(self, xx, xt):
-        y = self.pretrained(xx)
-        ytemp = self.pretrained(xt)
-        c = self.fc(torch.flatten(y-ytemp, 1))
-        return c
+        y1 = self.feature(xx)
+        ytemp1 = self.feature(xt)
+        y1 = self.pool(y1)
+        ytemp1 = self.pool(ytemp1)
+        pred = self.classifier(torch.flatten(y1-ytemp1, 1))
+        return pred
 
 
-if __name__ == "__main__":
-    # tainsform未生效
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Resize((224, 224)),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+def pretrain(trainRoot='./data/X_fabric_data/tr/', testRoot='./data/X_fabric_data/te/'):
 
-    train = FabricDataset(root='./data/DIY_fabric_data/train/')
-    test = FabricDataset(root='./data/DIY_fabric_data/test/')
+    train = FabricDataset(root=trainRoot)
+    test = FabricDataset(root=testRoot)
     train = DataLoader(train, batch_size=128, shuffle=True, num_workers=4)
     test = DataLoader(test, batch_size=128, shuffle=True, num_workers=4)
 
-    MAX_EPOCHES = 100
-    lr = 0.1
+    MAX_EPOCHES = 15
 
-    model = fabric()
+    model = fabric(15)
+
     model = model.to(device)
     if cuda.is_available():
-        model = torch.nn.DataParallel(model)
-    '''
-    opt1=torch.optim.Adam([
-            {'params':model.module.pretrained.parameters(),'lr':0.000001},
-            
-            {'params':model.module.fc.parameters(),'lr':0.01}
-            ])
-    '''
-    opt1 = torch.optim.Adam(model.parameters())
+        model = nn.DataParallel(model)
+    optimizer = optim.Adam(model.parameters())
     criterion = nn.CrossEntropyLoss()
+    logger.info('pretrain start training!')
     for epoch in range(MAX_EPOCHES):
-        for xx, xt, Y in train:
+        batch_idx = 0
+        loss_sum = 0.0
+        for X_gt, X_temp, Y in train:
 
-            xx = xx.to(device)
-            xt = xt.to(device)
-            c = model(xx, xt)
+            X_gt = X_gt.to(device)
+            X_temp = X_temp.to(device)
+            pred = model(X_gt, X_temp)
+
+            Y = Y.to(device)
+            loss = criterion(pred, Y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            loss_sum += loss
+            optimizer.step()
+            batch_idx += 1
+
+        with torch.no_grad():
+            right = 0
+            num = 0
+            for X_gt, X_temp, Y in test:
+                X_gt = X_gt.to(device)
+                X_temp = X_temp.to(device)
+                pred = model(X_gt, X_temp)
+                num = num+Y.shape[0]
+
+                right += (pred.argmax(dim=1) == Y.to(device)
+                          ).float().sum().cpu().item()
+            logger.info(
+                'Epoch:[{}/{}] batch_idx:{} loss={:.5f} accuracy={:.5f}'.format(epoch+1, MAX_EPOCHES, batch_idx, loss_sum/batch_idx, right/num))
+
+    if cuda.is_available():
+        return model.module.feature
+    else:
+        return model.feature
+
+
+def finetune(feature, trainRoot='./data/DIY_fabric_data/tr/', testRoot='./data/DIY_fabric_data/te/'):
+    model_root = os.path.join('./models', 'model.pt')
+    maxAccuracy = 0.0
+
+    train = FabricDataset(root=trainRoot)
+    test = FabricDataset(root=testRoot)
+    train = DataLoader(train, batch_size=128, shuffle=True, num_workers=4)
+    test = DataLoader(test, batch_size=128, shuffle=True, num_workers=4)
+
+    MAX_EPOCHES = 30
+
+    classifier = torchvision.models.AlexNet(3).classifier
+    avgpool = torchvision.models.AlexNet(3).avgpool
+
+    if os.path.exists(model_root) and feature == None:
+        checkpoit = torch.load(model_root)
+        maxAccuracy = checkpoit['maxAccuracy']
+        feature.load_state_dict(checkpoit['feature'])
+        classifier.load_state_dict(checkpoit['classifer'])
+
+    feature = feature.to(device)
+    avgpool = avgpool.to(device)
+    classifier = classifier.to(device)
+
+    if cuda.is_available():
+        feature = nn.DataParallel(feature)
+        avgpool = nn.DataParallel(avgpool)
+        classifier = nn.DataParallel(classifier)
+
+    optimizer = optim.Adam(classifier.parameters())
+    criterion = nn.CrossEntropyLoss()
+
+    logger.info("finetune start training!")
+    for epoch in range(MAX_EPOCHES):
+        batch_idx = 0
+        loss_sum = 0.0
+        for X_gt, X_temp, Y in train:
+
+            X_gt = X_gt.to(device)
+            X_temp = X_temp.to(device)
+
+            feature_gt = feature(X_gt)
+            feature_temp = feature(X_temp)
+            feature_gt = avgpool(feature_gt)
+            feature_temp = avgpool(feature_temp)
+            pred = classifier(torch.flatten(feature_gt-feature_temp, 1))
 
             Y[Y == 1] = 0
             Y[Y == 2] = 1
-            Y[Y == 5] = 2
-            Y[Y == 13] = 3
+            Y[Y == 14] = 2
             Y = Y.to(device)
-            '''
-            _onehot=torch.zeros((Y.shape[0],14),dtype=torch.int64)
-                
-            _onehot=_onehot.scatter_(1,_onehot,1)
-            _onehot=_onehot[:,[1,2,5,13]]
-            _onehot=_onehot.to(device)
-            '''
-            loss = criterion(c, Y)
 
-            opt1.zero_grad()
+            loss = criterion(pred, Y)
+
+            optimizer.zero_grad()
             loss.backward()
-
-            opt1.step()
-            print(loss.item())
+            loss_sum += loss
+            optimizer.step()
+            # logger.info(
+            #     'Epoch:[{}/{}] batch_idx:{} loss={:.5f}'.format(epoch, MAX_EPOCHES, batch_idx, loss))
+            batch_idx += 1
 
         with torch.no_grad():
 
             right = 0
-            su = 0
-            for X, Y, temp, xx, xt in test:
-                X = X.to(device)
-                temp = temp.to(device)
-                xx = xx.to(device)
-                xt = xt.to(device)
-                c = model(X, temp, xx, xt)
-                su = su+Y.shape[0]
+            sum = 0
+            for X_gt, X_temp, Y in test:
+                X_gt = X_gt.to(device)
+                X_temp = X_temp.to(device)
+
+                feature_gt = feature(X_gt)
+                feature_temp = feature(X_temp)
+                feature_gt = avgpool(feature_gt)
+                feature_temp = avgpool(feature_temp)
+                pred = classifier(torch.flatten(feature_gt-feature_temp, 1))
+
+                sum = sum+Y.shape[0]
 
                 Y[Y == 1] = 0
                 Y[Y == 2] = 1
-                Y[Y == 5] = 2
-                Y[Y == 13] = 3
-
-                right = right+(c.argmax(dim=1) == Y.to(device)
+                Y[Y == 14] = 2
+                right = right+(pred.argmax(dim=1) == Y.to(device)
                                ).float().sum().cpu().item()
 
-            print('test', epoch, right/su)
+            acc = right/sum
+            logger.info(
+                'Epoch:[{}/{}] batch_idx:{} loss={:.5f} accuracy={:.5f}'.format(epoch+1, MAX_EPOCHES, batch_idx, loss_sum/batch_idx, acc))
+
+            if acc > maxAccuracy:
+                maxAccuracy = acc
+                logger.info('acc:{:.5f} save model......'.format(acc))
+                state = {'feature': feature.state_dict(
+                ), 'classifier': classifier.state_dict(), 'maxAccuracy': acc}
+                torch.save(state, os.path.join('./models', 'model.pt'))
+
+
+if __name__ == "__main__":
+    data_root = 'data/fabric_data'
+    X_data_root = 'data/X_data'
+    DIY_data_root = 'data/DIY_data'
+    X_classes = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, ]
+    three_classes = [1, 2, 14]
+
+    # createDIYDataset(root=os.path.join('.', data_root),
+    #                  save=os.path.join('.',     X_data_root),
+    #                  classes=X_classes)
+    # createDIYDataset(root=os.path.join('.', data_root),
+    #                  save=os.path.join('.', DIY_data_root),
+    #                  classes=three_classes)
+    feature = pretrain(trainRoot=os.path.join(X_data_root, 'train'),
+                       testRoot=os.path.join(X_data_root, 'test'))
+    finetune(feature,
+             trainRoot=os.path.join(DIY_data_root, 'train'),
+             testRoot=os.path.join(DIY_data_root, 'test'))
